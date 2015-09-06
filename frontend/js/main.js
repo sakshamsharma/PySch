3a33b5f5b37e8cdff800dd13b27d49aac4347833
Saksham Sharma <saksham0808@gmail.com>
2015-09-06 13:28:46 +0530

    Sorting works.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,16 +1,26 @@
 var scroll=angular.module('PySch', []);
 
+
 scroll.controller('mainRaj', function($scope, $http) {
 	$scope.items = []
 	$scope.author = ""
 	$scope.authorcites = ""
 	$scope.authorh = ""
 	$scope.authori = ""
+	$scope.category = "author"
+	$scope.sorti = "cites"
+	$scope.loading = false
+
+	$scope.urlGen = function(inputstr) {
+		return 'http://localhost:8000/server/' + $scope.category + '/1?author=' + inputstr
+	}
 
 	$scope.checkKey = function(keyEvent) {
 		if (keyEvent.which === 13) {
+			$scope.loading = true;
 			inputstr = keyEvent.target.value;
-			val = $http.get('http://localhost:8000/server/author/1?author=' + inputstr);
+			url = $scope.urlGen(inputstr);
+			val = $http.get(url);
 			val.success(function(res) {
 				var data = JSON.parse(res);
 				$scope.items = data.papers;
@@ -18,8 +28,25 @@ scroll.controller('mainRaj', function($scope, $http) {
 				$scope.authorcites = data.cites;
 				$scope.authorh = data.hindex;
 				$scope.authori = data.iindex;
+				$scope.loading = false;
 			})
 		};
+	}
+
+	$scope.sorter = function() {
+		if($scope.sorti == "cites") {
+			$scope.items.sort(function(a,b) {
+				return a.cites - b.cites;
+			})
+		} else if($scope.sorti == "year") {
+			$scope.items.sort(function(a,b) {
+				return b.year - a.year;
+			})
+		}
+	}
+
+	$scope.cate = function(cate) {
+		$scope.category = cate;
 	}
 
 })
